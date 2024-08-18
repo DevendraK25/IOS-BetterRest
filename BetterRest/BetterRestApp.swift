e45e72faa9a3925e165e7962ef5964cf24d1c671
Devendra Kulkarni <devendrakulkarni@devendras-air.home>
2024-08-18 01:07:15 -0400

    Added BetterRest implementation

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Devendra Kulkarni on 2024-08-12.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
